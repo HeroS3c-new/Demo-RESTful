bc2d3c283a35220042b038001b6ab3c20122aa94
HeroS3c-new <126358473+HeroS3c-new@users.noreply.github.com>
2023-05-16 18:27:23 +0200

    Update documentazione.js

--- documentazione.js
+++ documentazione.js
@@ -4,8 +4,14 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./definizione_openapi.yaml');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(80, () => {
-  console.log('Server listening on port 80');
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
